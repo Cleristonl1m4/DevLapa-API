347fb6e14a6e36cb10cda983bb97f499c9aceec3
Cleriston <cleristonlima180600@gmail.com>
2026-03-08 22:48:45 -0300

    feat: add GET endpoint for fornecedores API

--- o-pai-o/src/main/java/com/devlapa/o_pai_o/service/FornecedoresServices.java
+++ o-pai-o/src/main/java/com/devlapa/o_pai_o/service/FornecedoresServices.java
@@ -2,12 +2,17 @@ package com.devlapa.o_pai_o.service;
 
 import com.devlapa.o_pai_o.domain.fornecedores.Fornecedores;
 import com.devlapa.o_pai_o.domain.fornecedores.FornecedoresRequestDTO;
+import com.devlapa.o_pai_o.domain.fornecedores.FornecedoresResponseDTO;
 import com.devlapa.o_pai_o.repositories.FornecedoresRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class FornecedoresServices {
@@ -30,4 +35,12 @@ public class FornecedoresServices {
     }
 
 
+    public List<FornecedoresResponseDTO> getFornecedores(int page, int size) {
+        Pageable pageable = PageRequest.of(page,size);
+        Page<Fornecedores> fornecedoresPage = this.fornecedores.findAll(pageable);
+        return fornecedoresPage.map(event -> new FornecedoresResponseDTO(event.getId(), event.getNome(),
+                event.getCnpj(),event.getTelefone(), event.getEmail(), event.getEndereco(), event.isAtivo(),
+                event.getData_cadrastro()))
+                .stream().toList();
+    }
 }
