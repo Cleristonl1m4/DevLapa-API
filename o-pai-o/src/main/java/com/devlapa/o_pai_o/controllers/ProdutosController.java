0d66b0f06ab5c1c5ed39664902ffc7869e2dd187
Cleriston <cleristonlima180600@gmail.com>
2026-03-15 20:24:19 -0300

    feat: Add GET endpoint for Produto

--- o-pai-o/src/main/java/com/devlapa/o_pai_o/controllers/ProdutosController.java
+++ o-pai-o/src/main/java/com/devlapa/o_pai_o/controllers/ProdutosController.java
@@ -3,14 +3,15 @@ package com.devlapa.o_pai_o.controllers;
 import com.devlapa.o_pai_o.domain.categorias.Categorias;
 import com.devlapa.o_pai_o.domain.produtos.Produtos;
 import com.devlapa.o_pai_o.domain.produtos.ProdutosRequestDTO;
+import com.devlapa.o_pai_o.domain.produtos.ProdutosResponseDTO;
 import com.devlapa.o_pai_o.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/produtos")
 public class ProdutosController {
@@ -24,5 +25,10 @@ public class ProdutosController {
             return ResponseEntity.ok(newProduto);
     }
 
+    @GetMapping
+    public ResponseEntity<List<ProdutosResponseDTO>> getProdutosList(@RequestParam(defaultValue = "0") int page, @RequestParam (defaultValue = "10") int size){
+        List<ProdutosResponseDTO> allProdutos = this.produtoService.getProdutos(page,size);
+        return ResponseEntity.ok(allProdutos);
+    }
 
 }
--- o-pai-o/src/main/java/com/devlapa/o_pai_o/service/ProdutoService.java
+++ o-pai-o/src/main/java/com/devlapa/o_pai_o/service/ProdutoService.java
@@ -4,10 +4,14 @@ import com.devlapa.o_pai_o.domain.categorias.Categorias;
 import com.devlapa.o_pai_o.domain.fornecedores.Fornecedores;
 import com.devlapa.o_pai_o.domain.produtos.Produtos;
 import com.devlapa.o_pai_o.domain.produtos.ProdutosRequestDTO;
+import com.devlapa.o_pai_o.domain.produtos.ProdutosResponseDTO;
 import com.devlapa.o_pai_o.repositories.CategoriasRepository;
 import com.devlapa.o_pai_o.repositories.FornecedoresRepository;
 import com.devlapa.o_pai_o.repositories.ProdutosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -45,5 +49,14 @@ public class ProdutoService {
         newProdutos.PrePersist();
         produtosRepository.save(newProdutos);
         return newProdutos;
+    }
+
+    public List<ProdutosResponseDTO> getProdutos(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Produtos> produtosPage = this.produtosRepository.findAll(pageable);
+        return produtosPage.map(event -> new ProdutosResponseDTO((long)Math.toIntExact(event.getId()), event.getNome(),
+                event.getPreco(), event.getUnidade(),event.getCategoria(),event.getFornecedor(),event.getAtivo(), event.getDataCriacao()))
+                .stream()
+                .toList();
     }
 }
